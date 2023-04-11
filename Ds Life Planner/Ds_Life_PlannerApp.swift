582b410018e9a977fef13422858f8554e7744f9f
BoiseITGuru <dev@boiseitguru.com>
2023-04-11 11:17:32 -0600

    iOS Project Created

---
+++ Ds Life Planner/Ds_Life_PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ds_Life_PlannerApp.swift
+//  Ds Life Planner
+//
+//  Created by BoiseITGuru on 4/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Ds_Life_PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
